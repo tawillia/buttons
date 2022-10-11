916beeb846e4eb415f53b98e8a2624172ce01181
Tatiana <35499334+tawillia@users.noreply.github.com>
2022-10-10 20:40:50 -0400

    Style update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col, Button } from 'react-bootstrap';
@@ -6,28 +7,59 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 
 
 function App() {
+  const MAX_LENGTH = 26 
+  let text="This text can be up to 40 characters!!!"
+  let width = 900;
+
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+  function getWindowSize() {
+    const {innerWidth, innerHeight} = window;
+    return {innerWidth, innerHeight};
+  }
+
   return (
     <div className="App">
-      <Container className='content-box-md'>
-      <Row className='nowrap'>
-        {/* <div> */}
-        <Col xs sm="3" md="3" lg="3" className='d-inline-block'>
-          <Button className='d-inline-block' limit = {10}>This text can be up to 40 characters!!!</Button>{" "}
+     
+      <Container fluid="xs" className='content-box-md'>
+    
+        <Row className='nowrap'>
+          <Col xs sm="8" md="8" lg="8" className='pe-1 d-inline-block'>
+             {windowSize.innerWidth < width ?
+        (
+          <>
+            <Button className="me-3 mb-3">{`${text.substring(0, MAX_LENGTH)}...`}</Button>
+          </>    
+        ) :
+        <Button className="me-3">{text}</Button>  
+      }
+       {windowSize.innerWidth < width ?
+        (
+          <>
+            <Button className="me-3 mb-3">{`${text.substring(0, MAX_LENGTH)}...`}</Button>{" "}
+          </>    
+        ) :
+        <Button className="me-3">{text}</Button>    
+      }
           </Col>
-        <Col xs sm="3" md="3" lg="3" className='d-inline-block'>
-          <Button className='d-inline-block'>This text can be up to 40 characters!!!</Button>
-        </Col>
-        <Col md="auto">
-        </Col>
- 
-        <Col xs sm="4" md="4" lg="4" className='align-right d-inline-block'>
-        <Button className='align-right d-inline-block'>Save</Button>{" "}
-        <Button className='lnk-btn align-right d-inline-block' variant="link">Cansel</Button>{" "}  
-        </Col>
   
-      </Row>
-
-      
+          <Col xs sm="4" lg="4" className='align-right d-inline-block'>
+            <Button className='align-right d-inline-block'>Save</Button>{" "}
+            <Button className='lnk-btn align-right d-inline-block me-3' variant="link">Cancel</Button>{" "}  
+          </Col>
+        </Row>
       </Container>
    
     </div>
